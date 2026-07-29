b7cf61c2f84c8d830202e7ca9f5ca18581603e4f
Victoria <victoria@dffrntlab.co>
2026-07-29 17:40:34 +0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/present-simple/',
   plugins: [
     vue(),
     vueDevTools(),
